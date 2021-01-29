c195681bec21c35bd7bea5fcc73a3a484712fac3
Christian Farris <christianfarriscs@gmail.com>
2021-01-28 20:42:36 -0600

    create character types

---
+++ apollo-server-auth-directive/src/types/characters.js
@@ -0,0 +1,26 @@
+import {gql} from 'apollo-server'
+
+const characterTypes = gql`
+  extend type Query {
+    character(id: ID!): Character
+    characters: [Character]!
+  }
+
+  extend type Mutation {
+    createCharacter(input: CharacterInput!): Character!
+    deleteCharacter(id: ID!): Boolean!
+  }
+
+  type Character {
+    id: ID!
+    name: String!
+    manga: String! @isAuthenticated
+  }
+
+  input CharacterInput {
+    name: String!
+    manga: String!
+  }
+`
+
+export default characterTypes
